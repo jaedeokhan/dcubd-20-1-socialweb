18f40a1275f3216479e9f3cb730fb3401b87bed9
jaedeokhan <dgd03023@cu.ac.kr>
2020-06-12 10:52:31 +0900

    2020 0612 FRI LDA 토픽 모델링

--- Crawling/crawling_data/save_file.py
+++
@@ -1,6 +0,0 @@
-def save_news_file(news_datas):
-    file = open("naver_news.csv", mode="w", encoding='utf-8', newline='')
-    writer = csv.writer(file)
-    writer.writerow(["인덱스", "검색어", "날짜", "신문사", "제목", "내용"])
-    for news in news_datas:
-        writer.writerow(list(news.values()))
